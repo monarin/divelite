4cc6ae6d373ca8804bc10dfb595fdb4cf2a5a884
monarin <monarin@gmail.com>
2018-10-30 18:36:31 -0700

    Add cython smdreader standalone for testing.

---
+++ cython/smd/dummy.py
@@ -0,0 +1,27 @@
+from smdreader import DummyReader
+import os, glob, sys, time
+from psana import dgram
+
+xtc_files = glob.glob("/reg/d/psdm/xpp/xpptut15/scratch/mona/test/smalldata/data-*.smd.xtc")
+#xtc_files = glob.glob(".tmp/smalldata/data-*.smd.xtc")
+n_files = int(sys.argv[1])
+fds = [os.open(xtc_file, os.O_RDONLY) for xtc_file in xtc_files]
+
+# do this to move to dgram part of the xtc
+# since config doesn't have correct timestamp so the 
+# code won't work - won't happen in real situation
+configs = [dgram.Dgram(file_descriptor=fd) for fd in fds]
+
+st = time.time()
+smdr = DummyReader(fds[:n_files])
+got_events = -1
+processed_events = 0
+while got_events != 0:
+    smdr.get(1000)
+    got_events = smdr.got_events
+    processed_events += got_events
+    for i in range(n_files):
+        view = smdr.view(i)
+
+en = time.time()
+print("processed_events %d total elapsed %f s rate %f MHz"%(processed_events, en-st, processed_events/((en-st) * 1000000)))
