091b6ca6d20069b908fef5e4bd05e9842bd9fd02
monarin <monarin@gmail.com>
2018-12-14 18:35:47 -0800

    update path

--- tests/lcls2/run.sh
+++ tests/lcls2/run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 source $HOME/lcls2/setup_env.sh
 conda activate ps-1.0.3
-python $HOME/lcls2/psana/psana/tests/smdreader.py
+python $HOME/lcls2/psana/psana/tests/dev_eventbuilder.py
 #python test_mpi.py
