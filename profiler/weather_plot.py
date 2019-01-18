b26d6be0f8a58ca3f3f72c3322c8413b2e715559
monarin <monarin@gmail.com>
2019-01-18 11:00:08 -0800

    Add deltas histogram.

--- profiler/weather_plot.py
+++ profiler/weather_plot.py
@@ -2,10 +2,11 @@ import sys
 import numpy as np
 import matplotlib.pyplot as plt
 
-size = int(sys.argv[1])
+log_fname = sys.argv[1]
+size = int(sys.argv[2])
 
 data = {}
-with open('xx') as f:
+with open(log_fname) as f:
     for line in f:
         cols = line.split()
         if len(cols) == 2:
@@ -16,10 +17,19 @@ with open('xx') as f:
             else:
                 data[rank].append(ts)
 
+deltas = []
 for i in range(1, size):
     if i in data:
         plt.scatter([i]*len(data[i]), data[i], s=2, marker='o')
+        # calculate delta
+        ts_list = data[i]
+        for j in range(len(ts_list)-1):
+            deltas.append(ts_list[j+1] - ts_list[j])
 
 plt.show()
 
-
+# plot histogram of deltas
+deltas = np.asarray(deltas)
+plt.hist(deltas, bins=100)
+plt.title("No. points %d More than 0.5s %d Min %f Max %f Mean %f"%(deltas.size, deltas[deltas>0.5].size, np.min(deltas), np.max(deltas), np.average(deltas)))
+plt.show()
