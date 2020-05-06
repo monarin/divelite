9604dda02f81d00f2a960db3432bf0f6255b0f0f
Monarin Uervirojnangkoorn <monarin@gmail.com>
2020-05-06 11:00:42 -0400

    better plot

--- profiler/weather_plot.py
+++ profiler/weather_plot.py
@@ -10,26 +10,42 @@ with open(log_fname) as f:
     for line in f:
         cols = line.split()
         if len(cols) == 2:
-            rank = int(cols[0])
-            ts = float(cols[1])
-            if rank not in data:
-                data[rank] = [ts]
-            else:
-                data[rank].append(ts)
+            try:
+                rank = int(cols[0])
+                ts = float(cols[1])
+                if rank not in data:
+                    data[rank] = [ts]
+                else:
+                    data[rank].append(ts)
+
+            except:
+                print(f'unexpected two columns data: {line}')
+                pass
+deltas = []
+for rank, ts_list in data.items():
+    min_ts=min(ts_list)
+    max_ts=max(ts_list)
+    deltas.append(len(ts_list))
+
+deltas = np.asarray(deltas)
+plt.hist(deltas, bins=100)
+thres = np.average(deltas) + ( np.std(deltas) )
+plt.title(f"No. points more than {thres:.2f} s: {len(deltas[deltas>thres]):d}  avg={np.average(deltas):.2f} max={np.max(deltas):.2f} min={np.min(deltas):.2f}")
+plt.show()
 
 deltas = []
-for i in range(1, size):
-    if i in data:
-        plt.scatter([i]*len(data[i]), data[i], s=2, marker='o')
-        # calculate delta
-        ts_list = data[i]
-        for j in range(len(ts_list)-1):
-            deltas.append(ts_list[j+1] - ts_list[j])
+for rank, ts_list in data.items():
+    if rank < 2000:
+        plt.scatter([rank]*len(ts_list), ts_list, s=2, marker='o')
+    # calculate delta
+    ts_arr = np.asarray(ts_list)
+    deltas.extend(list(ts_list - np.roll(ts_list, 1))[1:])
 
 plt.show()
 
 # plot histogram of deltas
 deltas = np.asarray(deltas)
 plt.hist(deltas, bins=100)
-plt.title("No. points %d More than 0.5s %d Min %f Max %f Mean %f"%(deltas.size, deltas[deltas>0.5].size, np.min(deltas), np.max(deltas), np.average(deltas)))
+thres = np.average(deltas) + ( np.std(deltas) )
+plt.title(f"No. points more than {thres:.2f} s: {len(deltas[deltas>thres]):d}  avg={np.average(deltas):.2f} max={np.max(deltas):.2f} min={np.min(deltas):.2f}")
 plt.show()
