3315bc5ba7eca8863b7b610bf4425aa6b53f2890
monarin <monarin@gmail.com>
2020-05-21 10:21:17 -0700

    add timing and stats calculation

--- cython/parallel/ex_prange.pyx
+++ cython/parallel/ex_prange.pyx
@@ -1,6 +1,6 @@
 from cython.parallel import prange
 import numpy as np
-#from posix.time cimport timeval, gettimeofday
+from posix.time cimport timeval, gettimeofday
 import time
 
 
@@ -8,21 +8,18 @@ def do_prange(int n, int batch_size):
     cdef int i, j
     cdef int sum_i = 0
 
-    #cdef timeval tv_st, tv_en
-    #cdef unsigned long ut_st, ut_en
-    cdef double time_st, time_en
-    time_st = time.time()
+    cdef timeval tv_st, tv_en
+    cdef unsigned long ut_st, ut_en
+    gettimeofday(&tv_st, NULL)
     
-    #gettimeofday(&tv_st, NULL)
     for i in prange(n, nogil=True, schedule='static'):
         for j in range(batch_size):
             sum_i += 1
-    #gettimeofday(&tv_en, NULL)
-    #ut_st = 1000000 * tv_st.tv_sec + tv_st.tv_usec
-    #ut_en = 1000000 * tv_en.tv_sec + tv_en.tv_usec
-    #print(f'{ut_st} {ut_en} {ut_en - ut_st} {sum_i}')
-    time_en = time.time()
-    print(f'{time_st} {time_en} {time_en-time_st}')
+    
+    gettimeofday(&tv_en, NULL)
+    ut_st = 1000000 * tv_st.tv_sec + tv_st.tv_usec
+    ut_en = 1000000 * tv_en.tv_sec + tv_en.tv_usec
+    print(f'{ut_st} {ut_en} {ut_en - ut_st} {sum_i}')
 
 """
 
--- cython/parallel/tst.py
+++ cython/parallel/tst.py
@@ -1,4 +1,4 @@
 from ex_prange import do_prange
 
-for i in range(100):
-    do_prange(16, 1)
+for i in range(1000):
+    do_prange(16, 10000)
