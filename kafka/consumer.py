61bd4db412de23b5c3a95f81586c770f588f750a
Monarin Uervirojnangkoorn <monarin@gmail.com>
2024-03-11 17:41:35 -0700

    update kafka example

--- kafka/consumer.py
+++ kafka/consumer.py
@@ -4,8 +4,10 @@ import json
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
+bootstrap_server = "testfac-lgbkdb:9092"
+#bootstrap_server = "psdbdev01:9092"
 #consumer = KafkaConsumer(bootstrap_servers=["psdbdev01:9092"], group_id="monatest", max_poll_interval_ms=500000, max_poll_records=50)
-consumer = KafkaConsumer(bootstrap_servers=["psdbdev01:9092"], max_poll_interval_ms=500000, max_poll_records=50)
+consumer = KafkaConsumer(bootstrap_servers=[bootstrap_server], max_poll_interval_ms=500000, max_poll_records=50)
 consumer.topics()
 consumer.subscribe(["monatest"])
 for msg in consumer:
--- kafka/producer.py
+++ kafka/producer.py
@@ -6,7 +6,9 @@ from kafka import KafkaProducer
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
 
-producer = KafkaProducer(bootstrap_servers=["psdbdev01:9092"], value_serializer=lambda m:json.JSONEncoder().encode(m).encode('utf-8'))
+bootstrap_server = "testfac-lgbkdb:9092"
+#bootstrap_server = "psdbdev01:9092"
+producer = KafkaProducer(bootstrap_servers=[bootstrap_server], value_serializer=lambda m:json.JSONEncoder().encode(m).encode('utf-8'))
 for i in range(10):
     producer.send("monatest", {"hello": "world", "index": 1})
     time.sleep(1.0)
